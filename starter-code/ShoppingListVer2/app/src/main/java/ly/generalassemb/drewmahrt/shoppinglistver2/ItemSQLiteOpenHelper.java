80712990e626f47d9f66b90036e5db7da02d5d30
ECarrasco91 <ezequielcarrasco91@yahoo.com>
2016-10-25 22:18:11 -0400

    changes made

---
+++ starter-code/ShoppingListVer2/app/src/main/java/ly/generalassemb/drewmahrt/shoppinglistver2/ItemSQLiteOpenHelper.java
@@ -0,0 +1,74 @@
+package ly.generalassemb.drewmahrt.shoppinglistver2;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by student on 10/25/16.
+ */
+public class ItemSQLiteOpenHelper extends SQLiteOpenHelper {
+
+    public static final int DATABASE_VERSION = 7;
+    public static final String DATABASE_NAME = "SHOPPING_DB";
+    public static final String LIST_TABLE_NAME = "SHOPPING_LIST";
+
+    public static final String COL_ID = "_id";
+    public static final String COL_ITEM_NAME = "ITEM_NAME";
+
+    public static final String CREATE_LIST_TABLE =
+            "CREATE TABLE " + LIST_TABLE_NAME + " (" +
+                    COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    COL_ITEM_NAME + " TEXT)";
+
+    private static ItemSQLiteOpenHelper mInstance;
+
+    public static ItemSQLiteOpenHelper getInstance(Context context){
+        if (mInstance == null){
+            mInstance = new ItemSQLiteOpenHelper(context.getApplicationContext());
+        }
+        return mInstance;
+    }
+
+    private ItemSQLiteOpenHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_LIST_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + LIST_TABLE_NAME);
+        this.onCreate(db);
+    }
+
+    // Query the database
+    public List<ItemObject> getList(){
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.query(LIST_TABLE_NAME,
+                new String[]{COL_ITEM_NAME},
+                null, null, null, null, null);
+
+        List<ItemObject> itemList = new ArrayList<>();
+
+        if (cursor.moveToFirst()){
+            while (!cursor.isAfterLast()){
+                String name = cursor.getString(cursor.getColumnIndex(COL_ITEM_NAME));
+
+                ItemObject object = new ItemObject(name);
+                itemList.add(object);
+
+                cursor.moveToNext();
+            }
+        }
+        cursor.close();
+        return itemList;
+    }
+}
--- starter-code/ShoppingListVer2/app/src/main/java/ly/generalassemb/drewmahrt/shoppinglistver2/MainActivity.java
+++ starter-code/ShoppingListVer2/app/src/main/java/ly/generalassemb/drewmahrt/shoppinglistver2/MainActivity.java
@@ -4,15 +4,20 @@ import android.content.Context;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
+import java.util.List;
+
 import ly.generalassemb.drewmahrt.shoppinglistver2.setup.DBAssetHelper;
 
 public class MainActivity extends AppCompatActivity {
+    RecyclerView mRecyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +27,16 @@ public class MainActivity extends AppCompatActivity {
         DBAssetHelper dbSetup = new DBAssetHelper(MainActivity.this);
         dbSetup.getReadableDatabase();
 
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        LinearLayoutManager linearLayoutManager =
+                new LinearLayoutManager(MainActivity.this,
+                        LinearLayoutManager.VERTICAL, false);
+
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+
+        List<ItemObject> list = ItemSQLiteOpenHelper.getInstance(this).getList();
+
+        mRecyclerView.setAdapter(new ItemRecyclerViewAdapter(list));
     }
 }
